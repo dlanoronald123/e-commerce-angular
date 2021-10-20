80787af823a7fc47d6c1e606973f6007efd498ca
Ronald Magno <dlanoronald123@gmail.com>
2021-10-20 21:48:40 +0800

    Add styling to recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', "Sample Test kaibigan", "https://i1.sndcdn.com/artworks-54YNdfIjfngFhLtW-FyUCbw-t500x500.jpg"),
+    new Recipe('A test recipe', "Sample Test kaibigan", "https://i1.sndcdn.com/artworks-54YNdfIjfngFhLtW-FyUCbw-t500x500.jpg")
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
